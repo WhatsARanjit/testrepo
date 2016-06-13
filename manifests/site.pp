f0cbb8c32d706dbff38ad3303e3af708e05b2921
WhatsARanjit <whatsaranjit@gmail.com>
2016-06-13 14:14:08 +0000

    Changing test branch to canary_test for app orch

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,7 +11,7 @@ node default {
 
 site {
   canary::on { 'East Coast':
-    test_env  => 'preview',
+    test_env  => 'canary_test',
     test_node => 'node1.whatsaranjit.com',
     nodes => {
       Node['puppet2016.1.1.puppetlabs.vm'] => Canary::Group['East Coast'],
