87f2e16fec0bc58aa8cbd40e055dc59bad4203b0
WhatsARanjit <whatsaranjit@gmail.com>
2016-06-15 17:29:19 +0000

    Adding correct path to webapps

--- site/profiles/manifests/jenkins.pp
+++ site/profiles/manifests/jenkins.pp
@@ -10,7 +10,7 @@ class profiles::jenkins {
   }
 
   tomcat::war { 'jenkins.war':
-    catalina_base => '/opt/tomcat8/webapps',
+    catalina_base => '/opt/tomcat8',
     war_source    => 'http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war',
   }
 
