b64e31962d81ad62bc6721e259243464a493399d
Ranjit Viswakumar <WhatsARanjit@users.noreply.github.com>
2016-09-09 12:49:08 -0400

    Removed filebucket & app orch stuff

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,28 +1,5 @@
-filebucket { 'main':
-  server => $::settings::server,
-  path   => false,
-}
-
-File { backup => 'main' }
 Package { allow_virtual => true }
 
 node default {
-  ini_setting { 'use_cached_catalog':
-    ensure  => present,
-    path    => $settings::config,
-    section => 'agent',
-    setting => 'use_cached_catalog',
-    value   => 'true',
-  }
-}
-
-site {
-  myapp { 'PVD':
-    port => '8081',
-    nodes => {
-      Node['node1.whatsaranjit.com'] => MyApp::Web['PVD'],
-      Node['node2.whatsaranjit.com'] => MyApp::App['PVD'],
-    }
-  }
 }
 
